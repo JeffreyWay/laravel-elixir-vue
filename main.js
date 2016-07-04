4ec3fcfe99dc4dcdccd51299a48aded1de7af8a5
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-07-04 12:32:28 -0400

    Add config items once Elixir is ready

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var Elixir = require('laravel-elixir');
 
-Elixir.config.js.webpack.loaders.push({
-    test: /\.vue$/,
-    loader: 'vue'
-});
+Elixir.ready(() => {
+    Elixir.config.js.webpack.loaders.push({
+        test: /\.vue$/,
+        loader: 'vue'
+    });
 
-Elixir.config.js.webpack.babel = {
-    presets: ['es2015-webpack'],
-    plugins: ['add-module-exports', 'transform-runtime']
-};
+    Elixir.config.js.webpack.babel = {
+        presets: ['es2015-webpack'],
+        plugins: ['add-module-exports', 'transform-runtime']
+    };
+});
