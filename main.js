a7dd94762ca2b96a9288a30b9c60a0f7583d4350
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-08-22 10:07:20 -0400

    Merge config instead

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var Elixir = require('laravel-elixir');
 
-Elixir.ready(() => {
-    Elixir.config.js.webpack.loaders.push({
-        test: /\.vue$/,
-        loader: 'vue'
+Elixir.ready(function() {
+    Elixir.webpack.mergeConfig({
+        babel: {
+            presets: ['es2015'],
+            plugins: ['add-module-exports', 'transform-runtime'],
+        },
+        module: {
+            loaders: [{
+                test: /\.vue$/,
+                loader: 'vue'
+            }]
+        }
     });
-
-    Elixir.config.js.webpack.babel = {
-        presets: ['es2015'],
-        plugins: ['add-module-exports', 'transform-runtime']
-    };
 });
